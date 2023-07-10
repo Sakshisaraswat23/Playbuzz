4935ba1ed63ff7c5f4153a4987aefa9768e2860e
Sakshisaraswat23 <sakshikaruna16@gmail.com>
2023-07-10 22:29:02 +0530

    project

---
+++ client/src/store/store.js
@@ -0,0 +1,10 @@
+import create from 'zustand';
+
+// useAuthStore is a hook and create will make a central store
+export const useAuthStore = create((set) => ({
+    auth : {
+        username : '',
+        active : false
+    },
+    setUsername : (name) => set((state) => ({ auth : { ...state.auth, username : name }})) 
+}))
